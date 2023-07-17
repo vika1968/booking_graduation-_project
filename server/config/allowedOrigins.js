b8f429a30fb0d6e620593725693a9fb2df45fd91
vikag1968mama1942 <vika@tdnet.com>
2023-07-17 11:58:48 +0300

    Remove src/config from Git repository

--- server/config/allowedOrigins.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.allowedOrigins = void 0;
-exports.allowedOrigins = ["http://localhost:3000", "http://localhost:3001"];
-//export const allowedOrigins = ["https://moovie-booking-5fsj.onrender.com"];
