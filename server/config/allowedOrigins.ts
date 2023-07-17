7d22763e686442174a97655854196b7507363543
vikag1968mama1942 <vika@tdnet.com>
2023-07-17 21:49:42 +0300

    fortawesome

--- server/config/allowedOrigins.ts
+++ server/config/allowedOrigins.ts
@@ -1,3 +1,3 @@
-export const allowedOrigins = ["http://localhost:3000", "http://localhost:3001"];
+export const allowedOrigins = ["https://hotel-booking-user.onrender.com", "http://localhost:3000", "http://localhost:3001"];
 
 //export const allowedOrigins = ["https://moovie-booking-5fsj.onrender.com"];
