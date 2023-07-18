5ece307bf7c1c22ae2361328c7423f335e987095
vikag1968mama1942 <vika@tdnet.com>
2023-07-19 02:03:51 +0300

    Fixing Admin

--- server/config/allowedOrigins.js
+++ server/config/allowedOrigins.js
@@ -1,4 +1,4 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.allowedOrigins = void 0;
-exports.allowedOrigins = ["https://hotel-booking-user.onrender.com", "http://localhost:3000", "http://localhost:3001"];
+exports.allowedOrigins = ["https://hotel-booking-user.onrender.com", "https://hotel-booking-admin-lrtu.onrender.com", "http://localhost:3000", "http://localhost:3001"];
--- server/config/allowedOrigins.ts
+++ server/config/allowedOrigins.ts
@@ -1,2 +1,2 @@
-export const allowedOrigins = ["https://hotel-booking-user.onrender.com", "http://localhost:3000", "http://localhost:3001"];
+export const allowedOrigins = ["https://hotel-booking-user.onrender.com", "https://hotel-booking-admin-lrtu.onrender.com", "http://localhost:3000", "http://localhost:3001"];
 
